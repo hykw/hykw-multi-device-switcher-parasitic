d96fd7962aead4db7119376187853a21f4d7cfcb
hykw <hykw1234@gmail.com>
2014-08-05 14:09:48 +0900

    first version

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ hykw-multi-device-switcher-parasitic
 ====================================
 
 parasitic multi-device-switcher
+
+## Usage:
+    hykwMDSP::getDevice()
+      hykwMDSP::SP
+      hykwMDSP::TABLET
+      hykwMDSP::MOBILE
+        -> cellular phone
+      hykwMDSP::OTHERS
+        -> pc, bot, etc...
---
+++ main.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package HYKW Multi Device Switcher parasitic Plugin
+ * @version 0.1
+ */
+/*
+Plugin Name: HYKW Multi Device Switcher parasitic Plugin
+Plugin URI: https://github.com/hykw/hykw-multi-device-switcher-parasitic
+Description: Multi Device Switcher のデバイス判定機能を利用するだけのプラグイン
+Author: hitoshi-hayakawa
+Version: 0.1
+*/
+
+class hykwMDSP
+{
+  const SP = 'smart';
+  const TABLET = 'tablet';
+  const MOBILE = 'mobile';
+  const OTHERS = 'others';
+
+  public static function getDevice()
+  {
+    $obj = new Multi_Device_Switcher();
+    $device = $obj->device;
+    switch($device) {
+	case '':
+	  return self::OTHERS;
+	case self::SP:
+	case self::TABLET:
+	case self::MOBILE:
+	  return $device;
+    }
+    echo 1;
+    
+  }
+}
+
